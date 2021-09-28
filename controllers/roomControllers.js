791de5cc1f94d5379f020bfbd0fe5fa87b613803
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-29 00:42:25 +0700

    :alembic: performing update and delete function operation
    checked

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -59,4 +59,62 @@ const getSingleRoom = async (req, res) => {
   }
 };
 
-export { allRooms, newRoom, getSingleRoom };
+// update the details of a single room item
+const updateSingleRoom = async (req, res) => {
+  try {
+    let room = await Room.findById(req.query.id);
+
+    if (!room) {
+      res.status(404).json({
+        success: false,
+        error: `Cannot find room with this ID, check your ID!`,
+      });
+    }
+
+    // looking for the id first => the body that we going to change/update
+    room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      room,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: `Sorry cannot retrieve the data: ${error.message}`,
+    });
+  }
+};
+
+//! delete the details of a single room item
+const deleteSingleRoom = async (req, res) => {
+  try {
+    const room = await Room.findById(req.query.id);
+
+    if (!room) {
+      res.status(404).json({
+        success: false,
+        error: `Cannot find room with this ID, check your ID!`,
+      });
+    }
+
+    // looking for the id first => the body that we going to change/update
+    await room.remove();
+
+    res.status(200).json({
+      success: true,
+      message: "Room is successfully deleted!",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: `Sorry cannot retrieve the data: ${error.message}`,
+    });
+  }
+};
+
+export { allRooms, newRoom, getSingleRoom, updateSingleRoom, deleteSingleRoom };
--- pages/api/rooms/[id].js
+++ pages/api/rooms/[id].js
@@ -1,11 +1,17 @@
 import nextConnect from "next-connect";
 import dbConnect from "../../../config/dbConnect";
-import { getSingleRoom } from "../../../controllers/roomControllers";
+import {
+  getSingleRoom,
+  updateSingleRoom,
+  deleteSingleRoom,
+} from "../../../controllers/roomControllers";
 
 const handler = nextConnect();
 
 dbConnect();
 
 handler.get(getSingleRoom);
+handler.put(updateSingleRoom);
+handler.delete(deleteSingleRoom);
 
 export default handler;
