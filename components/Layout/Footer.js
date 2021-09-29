c77c254bead27a24389dd5f7b660a9c9dee8ac9b
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-30 00:55:11 +0700

    :art: templating the Header and Footer component
    checked

---
+++ components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="py-1">
+      <p className="text-center mt-1">
+        Book IT - &copy; 2021, All Rights Reserved
+      </p>
+    </footer>
+  );
+};
