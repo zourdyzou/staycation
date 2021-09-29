4ecc928b2e5ed3fc315e513744b45d4c5782575f
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-30 01:09:36 +0700

    :fire: Layouting the content -> SEO best practices
    checkedd

---
+++ components/Layout/Layout.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Head from "next/head";
+
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+
+export const Layout = ({
+  children,
+  title = "Book Best Hotels for your holiday!",
+  description = "Welcome to our site!, where you can find the beauty of our world!",
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
