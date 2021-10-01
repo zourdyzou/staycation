c6b8c8fd0618f46037b9770c9620404df2bfe960
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-10-02 01:41:32 +0700

    :camera_flash: setup cloudinary for uploading avatar image
    checked

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,11 +1,26 @@
+import cloudinary from "cloudinary";
 import User from "../models/user";
 
 // import ErrorBoundary from "../utils/errorBoundary";
 import catchAsyncError from "../middlewares/catchAsyncError";
 // import APIFeatures from "../utils/apiFeatures";
 
+//! cloudinary config
+cloudinary.v2.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_API_SECRET,
+});
+
 // REGISTER USER => /api/rooms/register
 const registerUser = catchAsyncError(async (req, res) => {
+  // CLOUDINARY
+  const results = cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: "staycation/avatars",
+    width: "150",
+    crop: "scale",
+  });
+
   const { name, email, password, role } = req.body;
 
   const user = await User.create({
@@ -13,8 +28,8 @@ const registerUser = catchAsyncError(async (req, res) => {
     email,
     password,
     avatar: {
-      public_id: "PUBLIC",
-      url: "URL",
+      public_id: results.public_id,
+      url: results.secure_url,
     },
     role,
   });
