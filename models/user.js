92788f4f05b23ff9fdc9fa0736b023ca079660d2
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-10-03 15:58:47 +0700

    :building_construction: generate token for password recovery
    checkedd

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -54,6 +55,23 @@ userSchema.pre("save", async function (next) {
 // COMPARE USER PASSWORD
 userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// GENERATE TOKEN => resetting password
+userSchema.methods.getResetPasswordToken = function () {
+  // generate token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // hash and set to resetPassword token field
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // set token expire time
+  this.resetPasswordExpire = Date.now() * 30 * 60 * 1000;
+
+  return resetToken;
 };
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
--- redux/actions/userAction.js
+++ redux/actions/userAction.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 // import absoluteUrl from "next-absolute-url";
 
 import {
