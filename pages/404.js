49815d6330d5138871bfa9d04380616286dc93d4
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-10-11 01:19:38 +0700

    :building_construction: create 404 not found page
    checekd

---
+++ pages/404.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Layout } from "../components/Layout/Layout";
+
+const NotFoundPage = () => {
+  return (
+    <Layout title="404 The page is not found | Staycation">
+      <main className="max-w-6xl mx-auto pt-24 pb-0 md:py-24">
+        <div className="flex flex-col justify-center items-center transform translate-x-10 sm:translate-x-0">
+          <div className="relative h-[400px] w-[500px] transform translate-x-5 sm:translate-x-0">
+            <Image
+              className="bg-cover bg-center"
+              src="/animations/404.gif"
+              alt="page is not found | staycation"
+              layout="fill"
+            />
+          </div>
+
+          <div className="flex flex-col justify-center items-center space-y-9 pt-5 transform translate-x-5 sm:translate-x-0">
+            <h2 className="md:px-60 text-center text-xl">
+              Ooops! The page that you are visited is either not found or
+              currently in the development process.
+            </h2>
+
+            <Link passHref href="/">
+              <button className="px-6 py-3 w-52 flex justify-center bg-indigo-600 text-white rounded-lg hover:bg-indigo-800 transition ease-in duration-150">
+                Back To Home
+              </button>
+            </Link>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-// import { Home } from "../containers/Home";
 import { Layout } from "../components/Layout/Layout";
 
 import { getRooms } from "../redux/actions/roomAction";
