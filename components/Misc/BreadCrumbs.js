065dc7aa0066111c3916ae5b68785d0c34a78ce2
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-10-13 07:33:38 +0700

    :fire: remove unnecessary code
    checked

--- components/Misc/BreadCrumbs.js
+++ components/Misc/BreadCrumbs.js
@@ -5,9 +5,9 @@ import Link from "next/link";
 export const BreadCrumbs = ({ link }) => {
   return (
     <div className="bg-indigo-100">
-      <main className="max-w-6xl mx-auto pt-24 pb-0 md:py-24 px-6">
+      <main className="max-w-6xl mx-auto pt-24 pb-0 md:py-28 px-6">
         <div className="flex items-center justify-start transform translate-y-8">
-          <div className="flex items-center space-x-5">
+          <div className="flex items-center space-x-6">
             <Link passHref href="/">
               <span className="text-gray-400 hover:underline cursor-pointer">
                 Home
