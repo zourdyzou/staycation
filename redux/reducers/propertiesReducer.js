a6f1a6ec4edc55ddd66056730be1907485860d90
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-10-16 01:04:20 +0700

    :fire: remove dead code
    checked

--- redux/reducers/propertiesReducer.js
+++ redux/reducers/propertiesReducer.js
@@ -1,10 +1,4 @@
-import {
-  ALL_PROPERTIES_FAIL,
-  ALL_PROPERTIES_SUCCESS,
-  CLEAR_ERRORS,
-  PROPERTIES_DETAILS_FAIL,
-  PROPERTIES_DETAILS_SUCCESS,
-} from "../constants/propertiesConstant";
+import { CLEAR_ERRORS } from "../constants/bookingConstant";
 
 import {
   PAGE_PAGINATION_NUMBER,
@@ -23,52 +17,6 @@ export const paginationReducer = (state = { page: 0 }, action) => {
     case PAGE_PAGINATION_FAIL:
       return {
         loading: false,
-        error: action.payload,
-      };
-
-    case CLEAR_ERRORS:
-      return {
-        error: null,
-      };
-
-    default:
-      return state;
-  }
-};
-
-export const getPropertiesReducer = (state = { properties: [] }, action) => {
-  switch (action.type) {
-    case ALL_PROPERTIES_SUCCESS:
-      return {
-        loading: false,
-        properties: action.payload,
-      };
-
-    case ALL_PROPERTIES_FAIL:
-      return {
-        loading: false,
-        error: action.payload,
-      };
-
-    case CLEAR_ERRORS:
-      return {
-        error: null,
-      };
-
-    default:
-      return state;
-  }
-};
-
-export const propertiesDetailsReducer = (state = { property: {} }, action) => {
-  switch (action.payload) {
-    case PROPERTIES_DETAILS_SUCCESS:
-      return {
-        property: action.payload,
-      };
-
-    case PROPERTIES_DETAILS_FAIL:
-      return {
         error: action.payload,
       };
 
